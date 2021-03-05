b0b518cbb2838462f21ddeb57c47c35409039285
etff <wish2rich@gmail.com>
2021-03-05 22:28:20 +0900

    feature(currency): add update currency

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -2,10 +2,31 @@ var main = {
     init : function() {
         var _this = this;
 
+        $("#target").on("change", function() {
+            _this.retrieve();
+        });
+
         _this.load();
     },
     load : function() {
         var unit = "KRW";
+        $.ajax({
+            type: 'GET',
+            url: '/currency',
+            dataType: 'text',
+            data: { target : unit},
+            contentType:'application/json; charset=utf-8',
+        }).done(function(data) {
+            var result = JSON.parse(data);
+            var amount = result.amount.replace(/['"]+/g, '');
+            $("#rate").html(`환율 : ${amount} ${unit}/USD`);
+
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    retrieve: function() {
+        var unit = $("#target").val();
         $.ajax({
             type: 'GET',
             url: '/currency',
